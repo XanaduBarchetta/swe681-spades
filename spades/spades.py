7b04e86f73b161156a3524c1d1d4669b9eaecf91
Omar Farooq <omarfarooq93@gmail.com>
2020-05-08 23:51:46 -0400

    Added proper logging.

--- spades/spades.py
+++ spades/spades.py
@@ -29,32 +29,42 @@ def login():
         failed_validation = False
         if not isinstance(username, str) or not isinstance(password, str) or len(username) < 1 or len(password) < 1:
             flash('You must provide both a username and password.')
+            logger.error("User has not provided both a username and password")
             failed_validation = True
         if len(username) > 32:
             flash("Usernames can not be longer than 32 characters.")
+            logger.error("User %s has inputted a name longer than 32 characters.", username)
             failed_validation = True
         if not USERNAME_REGEX.match(username):
             flash("Usernames may only contain letters, numbers, and underscore.")
+            logger.error(
+                "User %s has inputted a name which does not contain letters, numbers, and underscore.",
+                username)
             failed_validation = True
         if len(password) < app.config['MIN_PASSWORD_LENGTH'] or len(password) > app.config['MAX_PASSWORD_LENGTH']:
             flash("Passwords are no fewer than {min} and no greater than {max} characters.".format(
                 min=app.config['MIN_PASSWORD_LENGTH'],
                 max=app.config['MAX_PASSWORD_LENGTH']
             ))
+            logger.error("User %s has inputted invalid password characteristics.", username)
             failed_validation = True
         if not PASSWORD_REGEX.match(password):
             flash("Passwords are limited to letters, numbers, and the following characters: -=+!@#$%^&*()_")
+            logger.error("User %s has inputted invalid password which does not meet -=+!@#$%^&*()_", username)
             failed_validation = True
         if failed_validation:
+            logger.error("There has been a failed validation.")
             return redirect(url_for('login'))
 
         user = User.get_user(username, password)
         if user is None:
             flash('The provided credentials do not match any registered user.')
+            logger.error("The provided credentials do not match any registered user for %s.", username)
             return redirect(url_for('login'))
         else:
             user.name = username
             login_user(user)
+            logger.info("Successful login for user %s.", username)
             return redirect(url_for('home', name=user.name))
     else:
         return render_template('login.html')
@@ -98,21 +108,26 @@ def signup():
         failed_validation = False
         if not isinstance(username, str) or not isinstance(password, str) or len(username) < 1 or len(password) < 1:
             flash('You must provide both a username and password.')
+            logger.error("Username and password was not provided.")
             failed_validation = True
         if len(username) > 32:
             flash("Usernames can not be longer than 32 characters.")
+            logger.error("Username was longer than 32 characters.")
             failed_validation = True
         if not USERNAME_REGEX.match(username):
             flash("Usernames may only contain letters, numbers, and underscore.")
+            logger.error("Username %s was not etters, numbers, or underscore.", username)
             failed_validation = True
         if len(password) < app.config['MIN_PASSWORD_LENGTH'] or len(password) > app.config['MAX_PASSWORD_LENGTH']:
             flash("Passwords must be no fewer than {min} and no greater than {max} characters.".format(
                 min=app.config['MIN_PASSWORD_LENGTH'],
                 max=app.config['MAX_PASSWORD_LENGTH']
             ))
+            logger.error("Password did not correct criteria.")
             failed_validation = True
         if not PASSWORD_REGEX.match(password):
             flash("Passwords are limited to letters, numbers, and the following characters: -=+!@#$%^&*()_")
+            logger.error("Password did not the following characterss: -=+!@#$%^&*()_.")
             failed_validation = True
         if failed_validation:
             return redirect(url_for('signup'))
@@ -121,9 +136,11 @@ def signup():
             User.create_user(username, password)
         except UserAlreadyExistsException:
             flash("Username already exists. Please choose a different username.")
+            logger.error("Username %s already exists.", username)
             return redirect(url_for('signup'))
         else:
             flash("You may now login with your new username and password.")
+            logger.info("Successful creation of username: %s", username)
             return redirect(url_for('login'))
     elif request.method == 'GET':
         return render_template("signup.html")
